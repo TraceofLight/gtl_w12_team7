65771d242928bdf8bcb23372dea1cfa39e3315ea
TraceofLight <rickyjun96@gmail.com>
2025-11-22 18:09:24 +0900

    chore: build warning
    소멸자 호출 관련 빌드 경고 처리

--- Mundi/Source/Runtime/Engine/Particle/ParticleDataContainer.h
+++ Mundi/Source/Runtime/Engine/Particle/ParticleDataContainer.h
@@ -38,3 +38,26 @@ struct FParticleDataContainer
 	// TODO: Add methods for memory allocation and deallocation
 	// TODO: Add methods for resizing and managing particle data
 };
+
+/**
+ * @brief 이미터 렌더 데이터의 기본 구조체
+ * @details 렌더링 스레드로 전달되는 파티클 이미터 데이터
+ *
+ * @param bValid 유효한 데이터인지 여부
+ * @param EmitterIndex 이미터 인덱스
+ */
+struct FDynamicEmitterDataBase
+{
+	bool bValid;
+	int32 EmitterIndex;
+
+	FDynamicEmitterDataBase()
+		: bValid(false)
+		, EmitterIndex(0)
+	{
+	}
+
+	virtual ~FDynamicEmitterDataBase()
+	{
+	}
+};
